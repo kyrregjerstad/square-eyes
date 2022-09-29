94a2246f485a8012ffe4e731337dc13a9a9632e8
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2022-09-29 11:10:21 +0200

    feat add subpages

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 import { carouselHandler } from "./carouselHandler.js";
 
 carouselHandler();
+
+const navbar = document.querySelectorAll(".pages a");
+
+navbar.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    navbar.forEach((item) => {
+      item.classList.remove("active");
+    });
+    event.currentTarget.classList.add("active");
+  });
+});
