9ebe69520cd8b48b9e03b0bbfe6a921f13cf313a
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2022-10-01 23:08:04 +0200

    feat add sidebar menu with background blur

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ navbar.forEach((item) => {
     event.currentTarget.classList.add("active");
   });
 });
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const closeButton = document.querySelector(".close-button");
+const backgroundBlur = document.querySelector(".background-blur");
+const sideBar = document.querySelector(".sidebar");
+
+hamburgerMenu.addEventListener("click", () => {
+  console.log("clicked");
+  sideBar.classList.toggle("hidden");
+  backgroundBlur.classList.toggle("hidden");
+});
+
+closeButton.addEventListener("click", () => {
+  console.log("click");
+  sideBar.classList.toggle("hidden");
+  backgroundBlur.classList.toggle("hidden");
+  // backgroundBlur.style.display = "none";
+});
+
+backgroundBlur.addEventListener("click", () => {
+  sideBar.classList.toggle("hidden");
+  backgroundBlur.classList.toggle("hidden");
+});
