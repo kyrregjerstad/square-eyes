91b5995982268bf8fe72a6b2afb122469c62c07d
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2022-10-20 16:03:21 +0200

    Refac add indentation to HTML in JS

--- scripts/populate_grid.js
+++ scripts/populate_grid.js
@@ -1,30 +1,28 @@
 import { movies } from "./database.js";
 
 export const tileBuilder = () => {
-  const tiles = document.querySelectorAll(".tile");
-
   let index = 0;
   let maxLength = movies.length;
+  const tiles = document.querySelectorAll(".tile");
+
   tiles.forEach((tile) => {
     tile.innerHTML = `
     <div class="tile-text">
-    <p class="movie-title">${movies[index].title}</p>
+      <p class="movie-title">${movies[index].title}</p>
     <div class="movie-year-genre">
-    <p class="movie-year">${movies[index].year}</p>
-    <p class="movie-dot"> • </p>
-    <p class="movie-genre">${movies[index].genre}</p>
+      <p class="movie-year">${movies[index].year}</p>
+      <p class="movie-dot"> • </p>
+      <p class="movie-genre">${movies[index].genre}</p>
     </div>
-    <p class="movie-description">
-    ${movies[index].description.substring(0, 90) + `...`}
-    </p>
+      <p class="movie-description">
+      ${movies[index].description.substring(0, 90) + `...`}
+      </p>
     </div>
     <button class="buy-now-button" data-token="${
       movies[index].token
     }">Buy Now</button>
     <div class="tile-fade"></div>
-    <img class="tile-image"
-    src="${movies[index].cover}"
-    alt=""/>
+    <img class="tile-image" src="${movies[index].cover}" alt=""/>
     `;
     index++;
     if (index === maxLength) {
