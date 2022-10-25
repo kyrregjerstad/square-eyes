a44f95b124f3665628e3b8142e9a50d7be444a7d
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2022-10-25 15:49:08 +0200

    feat add mobile view

--- scripts/script.js
+++ scripts/script.js
@@ -48,3 +48,35 @@ window.onresize = () => {
   clearTimeout(disableAnimation);
   disableAnimation = setTimeout(defaultAnimation, 100);
 };
+
+// move to mobile only script?
+
+const tile = document.querySelector(".mobile-banner");
+const title = document.querySelector(".hero-title");
+const button = document.querySelector(".buy-now");
+const description = document.querySelector(".mobile-hero-description");
+const backgroundImage = document.querySelector(".background-image");
+const frame = document.querySelector(".frame");
+// const mobileBackgroundBlur = document.querySelector(".mobile-background-blur");
+
+button.addEventListener("click", () => {
+  tile.classList.toggle("mobile-banner-active");
+  title.classList.toggle("hero-title-active");
+  description.classList.toggle("hero-description-active");
+  button.classList.toggle("buy-now-active");
+  frame.classList.toggle("frame-active");
+  backgroundImage.classList.toggle("background-image-active");
+  // mobileBackgroundBlur.classList.toggle("mobileBackgroundBlur-active");
+});
+
+/* set boolean based on screen size mobile: true, false
+
+if mobile = true
+if desktop = false
+
+add event listener to hero and tiles
+on click => add data atribute to class(es?) to transform to fullscreen
+on X click => remove data atribute to minimize
+on buy now click => play animation
+
+ */
