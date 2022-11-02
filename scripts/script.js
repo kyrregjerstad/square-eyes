d639fe9fb4b7db7f32a3eee21a107bbe8817eaf7
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2022-11-02 13:13:49 +0100

    refac encapsulate navbar styles in shadow dom

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ const qualitySelectorButtons = document.querySelectorAll(
 );
 templateSidebar();
 navbarDesktop();
-navbarMobile();
+// navbarMobile();
 mobileTileHandler();
 
 // templateMovieTile();
--- scripts/web-components/app_navbar-desktop.js
+++ scripts/web-components/app_navbar-desktop.js
@@ -1,61 +1,169 @@
 export const navbarDesktop = () => {
   const template = document.createElement("template");
 
-  template.innerHTML = `<link rel="stylesheet" href="style.css" />
-  <header>
-    <div class="navbar-top">
-        <div class="navbar-left">
-          <img
-            class="hamburger-menu"
-  })
-            src="images/icons/icon_menu_hamburger.svg"
-            alt="navigation menu"
-          />
-          <a href="/search.html">
-            <img src="images/icons/icon_menu_search.svg" alt="search icon"
-          /></a>
+  let style =
+    /* CSS */
+    `
+    <style>
+
+    .hidden {
+      display: none
+    }
+
+    a {
+      color: var(--default-black);
+      cursor: pointer;
+      text-decoration: none;
+    }
+
+    header {
+      position: relative;
+      background-color: var(--default-white);
+      z-index: 2000;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 100%;
+    }
+    
+    .navbar-top {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding-top: 0.5%;
+      width: 100%;
+      max-width: 1200px;
+      z-index: 1000;
+    }
+
+    .navbar-left, .navbar-right {
+      position: relative;
+      top: clamp(1px, 1.5vw, 34px);
+    }
+    
+    .navbar-left img {
+      height: clamp(15px, 2.5vw, 50px);
+      cursor: pointer;
+    }
+
+    .header-logo {
+      width: 18vw;
+      max-width: 350px;
+    }
+    
+    .user-icon {
+      height: clamp(15px, 4vw, 80px);
+    }
+    
+    .navbar-left-bottom {
+      margin-left: 1%;
+      position: absolute;
+      left: 0;
+    }
+    
+    .navbar-left-bottom img {
+      height: clamp(8px, 2vw, 50px);
+      cursor: pointer;
+    }
+    
+    .navbar-right-bottom img {
+      position: absolute;
+      top: 0;
+      right: 0;
+      height: clamp(8px, 4vw, 50px);
+      margin-right: 0.5%;
+    }
+    
+    .navbar-bottom {
+      position: sticky;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      top: 0;
+      height: clamp(16px, 4vw, 50px);
+      width: 100%;
+      background-color: var(--default-white);
+      box-shadow: var(--default-box-shadow-down);
+      z-index: 2000;
+    }
+    
+    .sticky {
+      position: fixed;
+      top: 0;
+    }
+    
+    .sticky-padding {
+      padding-top: 3vw;
+    }
+    
+    .pages {
+      display: flex;
+      justify-content: space-between;
+      width: 60vw;
+      max-width: 1180px;
+      font-size: var(--font-medium);
+      font-weight: 300;
+    }
+    
+    .active {
+      font-weight: bold;
+      text-decoration: underline;
+    }
+    </style>
+    `;
+
+  template.innerHTML =
+    /* HTML */
+    ` ${style}
+      <header>
+        <div class="navbar-top">
+          <div class="navbar-left">
+            <img
+              class="hamburger-menu"
+              src="images/icons/icon_menu_hamburger.svg"
+              alt="navigation menu"
+            />
+            <a href="/search.html">
+              <img src="images/icons/icon_menu_search.svg" alt="search icon"
+            /></a>
+          </div>
+          <div class="middle">
+            <img
+              class="header-logo"
+              src="/images/logo/SquareEyes_Logo_no_text.webp"
+              alt="Square Eyes logo"
+            />
+          </div>
+          <div class="navbar-right">
+            <img
+              class="user-icon"
+              src="images/icons/icon_menu_user.svg"
+              alt="user menu"
+            />
+          </div>
         </div>
-        <div class="middle">
-          <img
-            class="header-logo"
-            src="/images/logo/SquareEyes_Logo_no_text.webp"
-            alt="Square Eyes logo"
-          />
+        <div class="navbar-bottom">
+          <div class="navbar-left-bottom hidden">
+            <img
+              class="hamburger-menu"
+              src="images/icons/icon_menu_hamburger.svg"
+              alt="navigation menu"
+            />
+            <a href="/search.html">
+              <img src="images/icons/icon_menu_search.svg" alt="search icon"
+            /></a>
+          </div>
+          <nav class="pages">
+            <a href="/index.html" class="films">FILMS</a>
+            <a href="/browse.html" class="browse">BROWSE</a>
+            <a href="/collections.html" class="collections">COLLECTIONS</a>
+            <a href="/library.html" class="library">LIBRARY</a>
+          </nav>
+          <div class="navbar-right-bottom hidden">
+            <img src="images/icons/icon_menu_user.svg" alt="user menu" />
+          </div>
         </div>
-        <div class="navbar-right">
-          <img
-            class="user-icon"
-            src="images/icons/icon_menu_user.svg"
-            alt="user menu"
-          />
-        </div>
-    </div>
-    <div class="navbar-bottom">
-    <div class="navbar-left-bottom hidden">
-          <img
-            class="hamburger-menu"
-  })
-            src="images/icons/icon_menu_hamburger.svg"
-            alt="navigation menu"
-          />
-          <a href="/search.html">
-            <img src="images/icons/icon_menu_search.svg" alt="search icon"
-          /></a>
-        </div>
-      <nav class="pages">
-        <a href="/index.html" class="films">FILMS</a>
-        <a href="/browse.html" class="browse">BROWSE</a>
-        <a href="/collections.html" class="collections">COLLECTIONS</a>
-        <a href="/library.html" class="library">LIBRARY</a>
-      </nav>
-      <div class="navbar-right-bottom hidden">
-          <img
-            src="images/icons/icon_menu_user.svg"
-            alt="user menu"
-          />
-        </div>
-    </div>
-  </header>`;
+      </header>`;
 
   class NavbarDesktop extends HTMLElement {
     constructor() {
