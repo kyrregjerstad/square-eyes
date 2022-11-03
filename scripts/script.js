8ad464ec487462a82c9d7d5ca88a229d6a6ce64d
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2022-11-03 15:49:29 +0100

    refac update html template elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,17 @@
 import { carouselHandler } from "./carouselHandler.js";
-import { tileBuilder, checkoutTransfer } from "./populate_grid.js";
+// import { tileBuilder, checkoutTransfer } from "./populate_grid.js";
 import { navigation } from "./web-components/app_navigation.js";
-import { navbarMobile } from "./web-components/app_navbar-mobile.js";
 import { templateSidebar } from "./web-components/app_sidebar.js";
-import { isMobile } from "./helpers.js";
 import { templateMovieTile } from "./web-components/app_movie-tile.js";
+navigation();
+templateMovieTile();
+carouselHandler();
+checkoutTransfer();
+templateSidebar();
 
 const qualitySelectorButtons = document.querySelectorAll(
   ".quality-selector-button"
 );
-templateSidebar();
-navigation();
-// mobileTileHandler();
-
-templateMovieTile();
-
-carouselHandler();
-
-// tileBuilder();
-checkoutTransfer();
 
 qualitySelectorButtons?.forEach((button) => {
   button.addEventListener("click", () => {
